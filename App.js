45c8c30483f45b2d6bb608028a5c4a94a1180288
PhoeniXuzoo <PhoeniXuzoo@hotmail.com>
2020-05-15 11:57:39 -0500

    permission & camera work

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  state = {
+    hasPermission: null,
+    type: Camera.Constants.Type.back,
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasPermission } = this.state
+    if (hasPermission === null) {
+      return <View />;
+    }
+    else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    else {
+      return (
+          <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+              
+            </Camera>
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
